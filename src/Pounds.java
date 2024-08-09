c923825f4306e2d7047d998f611db3e500821e71
Joseph-ui-sketch <hernajos000@warren.k12.in.us>
2024-08-09 12:49:56 -0400

    Initial commit

---
+++ src/Pounds.java
@@ -0,0 +1,8 @@
+public class Pounds {
+    public static void main(String [] arg) {
+        var pounds = 200;
+        var kilos = pounds * 0.45359237;
+        System.out.println("kilograms:" + kilos);
+        System.out.println("pounds" + pounds);
+    }
+}
